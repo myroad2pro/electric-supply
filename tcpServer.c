efde295b2b0b589c3769af3d08bfa1d64d89b450
thelong0705 <thelong0705@gmail.com>
2018-05-25 20:17:38 +0700

    init with forking servers

---
+++ tcpServer.c
@@ -0,0 +1,88 @@
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 4444
+
+void sig_chld(int singno){
+        pid_t pid;
+        int stat;
+        while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
+                printf("child %d terminated\n", pid);
+        return;
+}
+
+int main(){
+
+	int sockfd, ret,n;
+	struct sockaddr_in serverAddr;
+
+	int newSocket;
+	struct sockaddr_in newAddr;
+
+	socklen_t addr_size;
+
+	char buffer[1024];
+	pid_t childpid;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0){
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	printf("[+]Server Socket is created.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("[-]Error in binding.\n");
+		exit(1);
+	}
+	printf("[+]Bind to port %d\n", 4444);
+
+	if(listen(sockfd, 10) == 0){
+		printf("[+]Listening....\n");
+	}else{
+		printf("[-]Error in binding.\n");
+	}
+
+
+	while(1){
+		newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+		if(newSocket < 0){
+			exit(1);
+		}
+		printf("Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+
+		if((childpid = fork()) == 0){
+			close(sockfd);
+			while( n = recv(newSocket, buffer, 1024, 0) > 0){
+			printf("Client: %s\n", buffer);
+			send(newSocket, buffer, n, 0);
+			bzero(buffer, sizeof(buffer));
+
+			}
+			close(newSocket);
+                        exit(0);
+		}
+		signal(SIGCHLD,sig_chld);
+		close(newSocket);
+
+	}
+
+	close(newSocket);
+
+
+	return 0;
+}
